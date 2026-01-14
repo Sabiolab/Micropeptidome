bb34b3691cbf46655fa79fe6ed7acab996affbe7
almoraco <amorac@cnio.es>
2026-01-14 09:31:48 +0100

    instructions for RNAseq

--- RNAseq/De_novo_transcripts.py
+++ RNAseq/De_novo_transcripts.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
-# check de novo transcripts GTF for novel transcripts without annotation
-# this file just prints the counts of annotated vs novel transcripts, to 
-# give a broad overview of the data
 import re
 from collections import defaultdict
+
+print(r"""
+          _____           _   _     _                      
+ _   _   |  __ \         | | (_)   | |                     
+| | | |  | |__) |__ _ __ | |_ _  __| | ___  _ __ ___   ___ 
+| | | |  |  ___/ _ \ '_ \| __| |/ _` |/ _ \| '_ ` _ \ / _ \
+| |_| |  | |  |  __/ |_) | |_| | (_| | (_) | | | | | |  __/
+| ___/   |_|   \___| .__/ \__|_|\__,_|\___/|_| |_| |_|\___|
+| |                | |                                       
+|_|                |_|                  almoraco 03/11/2025
+""")
+
+print(
+    "check de novo transcripts GTF for novel transcripts without annotation\n"
+    "this file just prints the counts of annotated vs novel transcripts\n"
+    "to give a broad overview of the data\n"
+)
 
 file_path = "merged.gtf" # path to the merged .gtf file that contains all the transcripts : merged.gtf
 
